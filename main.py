4c601a7f7bf35f13ae594e2e61cd32cce6aa36b3
zumassh <stasy.sl@yandex.ru>
2026-03-31 18:58:45 +0300

    fix: app start

--- main.py
+++ main.py
@@ -132,4 +132,6 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
